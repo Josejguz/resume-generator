3cce850f8aed11a067dc1b90c4e4631ba87f4d74
LAPTOP-EPEJ3SK5\jguzm <jguzmansanchez02@gmail.com>
2024-06-02 14:20:09 -0400

    Modify Navbar Styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,21 +1,23 @@
 import React from 'react';
+import { LightBulbIcon } from '@heroicons/react/16/solid';
 
 export default function Navbar() {
     return (
         <nav className='sticky top-0 left-0 z-50 h-20 bg-red-500'>
-            <div className='container mx-auto flex justify-between items-center p-4'>
-                <h1 className='text-2xl font-bold text-white'>Resumade</h1>
-                <ul className='flex space-x-4'>
+            <div className='container mx-auto flex justify-between items-center p-4 h-20'>
+                <a href='/'><h1 className='text-2xl font-bold text-white'>Resumade</h1></a>
+                <ul className='flex space-x-8 p-4'>
                     <li>
-                        <a href='#' className='text-white'>Home</a>
+                        <a href='#' className='text-white'>Templates</a>
                     </li>
                     <li>
-                        <a href='#' className='text-white'>About</a>
+                        <a href='#' className='text-white'>Tips</a>
                     </li>
                     <li>
-                        <a href='#' className='text-white'>Contact</a>
+                        <a href='#' className='text-white'>Build</a>
                     </li>
                 </ul>
+                <a href='#' className='bg-amber-400 px-3 py-1 rounded-2xl'><h3 className='text-white'>Login</h3></a>
             </div>
         </nav>
     )
