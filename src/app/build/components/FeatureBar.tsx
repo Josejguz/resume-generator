f6223cf986b4e64cfe173423181e143802003db6
LAPTOP-EPEJ3SK5\jguzm <jguzmansanchez02@gmail.com>
2024-06-02 18:04:43 -0400

    Create Feature Bar for Resume Building

---
+++ src/app/build/components/FeatureBar.tsx
@@ -0,0 +1,46 @@
+'use client'
+
+import React from "react";
+import { useState } from "react";
+import InformationTab from "./InformationTab";
+export default function FeatureBar() {
+
+    const [activeTab, setActiveTab] = useState("InformationTab");
+
+    const handleTabChange = (tab: string) => {
+        setActiveTab(tab);
+    }
+
+    const renderTab = () => {
+        switch(activeTab) {
+            case "InformationTab":
+                return <InformationTab />
+            case "StylingTab":
+                return <h1>Styling Tab</h1>
+            case "FormatTab":
+                return <h1>Format Tab</h1>
+            default:
+                return <InformationTab />
+        }
+    }
+
+    return (
+        <nav>
+            <div>
+                <ul className="flex justify-evenly border-b-2 border-white">
+                    <li>
+                        <button onClick={() => handleTabChange("InformationTab")} >Information</button>
+                    </li>
+                    <li>
+                        <button onClick={() => handleTabChange("StylingTab")}>Styling</button>
+                    </li>
+                    <li>
+                        <button onClick={() => handleTabChange("FormatTab")}>Format</button>
+                    </li>
+                </ul>
+                <br/>
+                {renderTab()} 
+            </div>
+        </nav>
+    )
+}
