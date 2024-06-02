10eea1420e99d16e3c77574fef6677df0d2f338a
LAPTOP-EPEJ3SK5\jguzm <jguzmansanchez02@gmail.com>
2024-06-02 16:27:32 -0400

    Create Build Page

--- src/app/build/page.tsx
+++ src/app/build/page.tsx
@@ -1,10 +1,27 @@
-import Navbar from "@/components/Navbar"
+import Navbar from "@/components/Navbar";
+import { PlusIcon } from "@heroicons/react/16/solid";
 
 export default function Home() {
     return (
         <div>
             <Navbar />
-            <h1>Build</h1>
+            <div className="flex flex-col mx-10 my-8">
+                <div>
+                    <h1 className="text-xl font-bold">Build Your Resume!</h1>
+                </div>
+                <br/>
+
+                <div>
+                    <button className="bg-red-500 hover:bg-red-700 py-3 px-4 text-white rounded-3xl"><span>New Resume <PlusIcon className="h-6"/></span></button>
+                </div>
+                <br />
+
+                <div>
+                    <h1>My Resumes</h1>
+                    <p className="text-xs text-gray-500 py-2">No Resumes Created</p>
+                </div>
+            </div>
+
         </div>
     )
 }
