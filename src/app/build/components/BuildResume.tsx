92fd22be16749ecb5825500063eea0d2f2152327
LAPTOP-EPEJ3SK5\jguzm <jguzmansanchez02@gmail.com>
2024-06-12 13:59:09 -0400

    Create Build Resume Feature

--- src/app/build/components/BuildResume.tsx
+++ src/app/build/components/BuildResume.tsx
@@ -4,14 +4,14 @@ import FeatureBar from './FeatureBar';
 
 export default function BuildResume() { 
     return (
-        <div className="border-red-500 border-2 rounded-lg text-white">
-            <h1 className=" font-bold border-b-white bg-red-500 text-2xl">Build Resume</h1>
+        <div className="border-red-500 border-2 rounded-lg w-2/3 ">
+            <h1 className=" font-bold border-b-white bg-red-500 text-2xl text-white">Build Resume</h1>
             <div className="flex flex-rows">
                 <div className="w-2/5">
                     <h1>Placeholder</h1>
                 </div>
 
-                <div className="bg-red-500 w-3/5 p-2">
+                <div className="w-3/5 p-2">
                     <FeatureBar />
                 </div>
 
--- src/app/build/components/InformationTab.tsx
+++ src/app/build/components/InformationTab.tsx
@@ -2,38 +2,48 @@
 
 export default function InformationTab() {
     return (
-        <div>
+        <div className="w-1/2">
             <form className="flex flex-col">
                 <h2 className="text-xl font-bold">Header</h2>
                 <label htmlFor="name">Name</label>
-                <input type="text" id="name" name="name" />
+                <input type="text" id="name" name="name" className="border-2 border-red-500" />
 
                 <label htmlFor="email">Email</label>
-                <input type="email" id="email" name="email" />
+                <input type="email" id="email" name="email" className="border-2 border-red-500" />
 
                 <label htmlFor="phone">Phone</label>
-                <input type="tel" id="phone" name="phone" />
+                <input type="tel" id="phone" name="phone" className="border-2 border-red-500"/>
 
                 <label htmlFor="linkedIn">LinkedIn</label>
-                <input type="text" id="address" name="address" />
+                <input type="text" id="address" name="address" className="border-2 border-red-500"/>
                 <br/>
 
                 <label htmlFor="Objective">Objective</label>
-                <textarea id="Objective" name="Objective" />
+                <textarea id="Objective" name="Objective" className="border-2 border-red-500"/>
                 <br/>
                         
                 <h2 className="text-xl font-bold">Education</h2>
                 <label htmlFor="School">School</label>
-                <input type="text" id="School" name="School" />
+                <input type="text" id="School" name="School" className="border-2 border-red-500"/>
 
                 <label htmlFor="Degree">Degree</label>
-                <input type="text" id="Degree" name="Degree" />
+                <input list="Degree" name="Degree" className="border-2 border-red-500"/>
+                <datalist id="Degree">
+                    <option value="Associate's"/>
+                    <option value="Bachelor's"/>
+                    <option value="Master's"/>
+                    <option value="PhD"/>
+                </datalist>
+
+                <label htmlFor="Major">Major</label>
+                <input type="text" id="Major" name="Major" className="border-2 border-red-500"/>
 
                 <label htmlFor="Graduation">Graduation Date</label>
-                <input type="date" id="Graduation" name="Graduation" />
+                <input type="date" id="Graduation" name="Graduation" className="border-2 border-red-500"/>
+
 
                 <label htmlFor="GPA">GPA</label>
-                <input type="number" id="GPA" name="GPA" />
+                <input type="number" id="GPA" name="GPA" className="border-2 border-red-500"/>
                 <br/>
 
                 <h2 className="text-xl font-bold">Experience</h2>
