b5e040d6154bb6f4f5c14b233930183aff24243e
LAPTOP-EPEJ3SK5\jguzm <jguzmansanchez02@gmail.com>
2024-06-14 23:07:52 -0400

    Change Build Resume Component Size & Add Help Button

--- src/app/build/components/BuildResume.tsx
+++ src/app/build/components/BuildResume.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import FeatureBar from './FeatureBar';
-
+import { InformationCircleIcon } from '@heroicons/react/16/solid';
 
 export default function BuildResume() { 
     return (
-        <div className="border-home border-2 rounded-lg w-2/3 ">
-            <h1 className=" font-bold border-b-white bg-home text-2xl text-white">Build Resume</h1>
+        <div className="border-home border-2 rounded-lg w-9/10 ">
+            <h1 className=" font-bold border-b-white bg-home text-2xl text-white p-5">Build Resume</h1>
             <div className="flex flex-rows">
                 <div className="w-2/5">
                     <h1>Placeholder</h1>
@@ -17,6 +17,10 @@ export default function BuildResume() {
 
                 <br/>
                 
+            </div>
+
+            <div className="flex p-3 justify-end bg-home">
+                <a href="/tips" className="flex flex-row justify-end text-white hover:text-gray-300 ">Need Help?< InformationCircleIcon className="h-5 w-5"/></a>
             </div>
         </div>
     )
