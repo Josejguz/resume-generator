d914a6e6e9b6da7b29680c63733c0d1444887f4f
LAPTOP-EPEJ3SK5\jguzm <jguzmansanchez02@gmail.com>
2024-06-18 19:30:40 -0400

    Receive Functions From FeatureBar Child Component

--- src/app/build/components/NewResume.tsx
+++ src/app/build/components/NewResume.tsx
@@ -1,18 +1,41 @@
 import React from 'react';
 import FeatureBar from './FeatureBar';
 import { InformationCircleIcon } from '@heroicons/react/16/solid';
+import InformationTab from './InformationTab';
+import { useState } from 'react';
 
 export default function NewResume() { 
+
+    const [activeTab, setActiveTab] = useState("InformationTab");
+
+    const handleTabChange = (tab: string) => {
+        setActiveTab(tab);
+    }
+
+    const renderTab = () => {
+        switch(activeTab) {
+            case "InformationTab":
+                return <InformationTab />
+            case "StylingTab":
+                return <h1>Styling Tab</h1>
+            case "FormatTab":
+                return <h1>Format Tab</h1>
+            default:
+                return <InformationTab />
+        }
+    }
+
     return (
         <div className="border-home border-2 rounded-lg w-9/10 ">
             <div className='flex flex-row w-auto bg-home justify-between items-center'>
                 <h1 className="font-bold text-2xl text-white ml-5">Build Resume</h1>
-                <FeatureBar />
+                <FeatureBar handleTabChange={handleTabChange} />
             </div>
             <div className="flex flex-rows">
                 <div className="w-2/5">
                     <h1>Placeholder</h1>
                 </div>
+                {renderTab()}
                 <br/>
                 
             </div>
