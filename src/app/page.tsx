95004c84660ca3330f8d28c742fe0b939f01fe96
LAPTOP-EPEJ3SK5\jguzm <jguzmansanchez02@gmail.com>
2024-06-19 13:45:56 -0400

    Create Home Page Dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
 import Image from "next/image";
 import Navbar from "@/components/Navbar";
+import Dashboard from "@/components/Dashboard";
 
 export default function Home() {
   return (
    <div>
       <Navbar />
+      <div className="flex justify-center items-center mt-20 flex-col">
+        <Dashboard />
+        <h1 className=" text-home mt-10 font-bold text-3xl">Welcome to Resumade</h1>
+        <br/>
+        <p className="text-gray-500">Want an easy way to build your resume? </p>
+      </div>
+      
    </div>
   );
 }
---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,7 @@
+export default function Dashboard() {
+    return (
+        <div className="flex border-2 border-black h-96 w-96">
+            <h1>Dashboard</h1>
+        </div>
+    )
+}
